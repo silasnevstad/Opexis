6f0b7878fdd69266125701a44c13c88096afc986
Silas Nevstad <nevstads@gmail.com>
2023-07-19 09:43:33 +0200

    Styling changes

--- src/components/AccountModal.js
+++ src/components/AccountModal.js
@@ -124,7 +124,8 @@ const AccountModal = ({ handleLogout, isSidebarOpen }) => {
                 </div>
                 <div className="big-modal-right">
                     <button className='big-close-button'type="button" onClick={() => setAccountModalOpen(false)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                        &times;
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg> */}
                     </button>
                     {curentPage === 'account' ? <AccountPage userEmail={userEmail} handleLogout={handleLogout} /> : <PreferencesPage userApiKey={userApiKey} settingApiKey={settingApiKey} setSettingApiKey={setSettingApiKey} apiKeyField={apiKeyField} setApiKeyField={setApiKeyField} handleConfirm={handleConfirm} />}
                 </div>
